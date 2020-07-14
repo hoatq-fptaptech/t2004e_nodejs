35ac1ed0b30bea9b3ea8ae5c048537d9c8f02c6c
billytrinh <quanghoa.trinh@gmail.com>
2020-07-14 19:15:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+app.listen(5000,function () {
+    console.log("server is running...");
+});
+app.use(express.static("public"));
+// sử dụng ejs làm view engine
+app.set("view engine","ejs");
+
+var counter = 0;
+app.get("/",function (req,res) {
+    counter++;
+    let city = req.query.cityname;
+    if(city === undefined){
+        city = "Hanoi,vietnam";
+    }
+    res.render("home",{
+        counter: counter,
+        city:city
+    });
+});
+
+app.get("/about-us",function (req,res) {
+    res.send("Gioi thieu ve chung toi");
+});
+const fs = require("fs");
+app.get("/danh-sach-thanh-pho",function (req,res) {
+    let data = fs.readFileSync("data/thanhpho.json","utf-8");
+    let cities = JSON.parse(data);
+    res.render("cities",{
+        cities:cities
+    })
+});
+
+app.get("/thanh-pho/:id",function (req,res) {
+    let cityId = req.params.id;
+    let city = {};
+    let data = fs.readFileSync("data/thanhpho.json","utf-8");
+    let cities = JSON.parse(data);
+    cities.map(function (e) {
+        if(e.id == cityId){
+            city = e;
+        }
+    });
+    res.render("city",{
+        city:city
+    });
+})
